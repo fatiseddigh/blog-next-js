23bbfeec1405d78447d7181b2badc11fa5bad249
fatiseddigh <seddighfati@gmail.com>
2023-07-04 12:26:46 +0430

    add context

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,13 @@
+import AuthProvider from "@/context/AuthContext";
 import "../../styles/globals.css";
 import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <AuthProvider>
       <Component {...pageProps} />;
       <Toaster />
-    </>
+    </AuthProvider>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import Layout from "@/containers/Layout";
+import { useAuth } from "@/context/AuthContext";
 
 const HomePage = () => {
+  const user = useAuth();
+  console.log(user);
   return (
     <Layout>
       <h1>this is home page</h1>
